f708e3cf4f92bb44099abd89b089ae1409258ca2
Konstantin Golubkin <konstantin.golubkin@hotmail.com>
2019-09-03 18:53:34 +0700

    new store elem

---
+++ src/store/modules/PopupMain.js
@@ -0,0 +1,74 @@
+export default {
+  state: {
+    questionnaire: [
+      {
+        type: 'select multi',
+        label: 'lorem 1',
+        questId: 'q1',
+        responses: [
+          { id: 1, text: 'aa1', isSelected: false },
+          { id: 2, text: 'aa2', isSelected: false },
+          { id: 3, text: 'aa3', isSelected: false }
+        ]
+      },
+      {
+        type: 'select one',
+        label: 'lorem 2',
+        questId: 'q2',
+        responses: [
+          { id: 4, text: 'bb1', isSelected: false },
+          { id: 5, text: 'bb2', isSelected: false },
+          { id: 6, text: 'bb3', isSelected: true }
+        ]
+      },
+      {
+        type: 'text only',
+        label: 'lorem 3',
+        questId: 'q3',
+        responses: ''
+      },
+      {
+        type: 'score',
+        label: 'lorem 4',
+        questId: 'q4',
+        responses: ''
+      }
+    ],
+    questionnaireIndex: 0
+  },
+  getters: {
+    GET_QUESTIONNAIRE: state => {
+      return state.questionnaire
+    },
+    GET_QUESTIONNAIRE_INDEX: state => {
+      return state.questionnaireIndex
+    }
+  },
+  mutations: {
+    MUTATE_QUESTIONNAIRE_INDEX (state, payload) {
+      state.questionnaireIndex = payload
+    },
+    MUTATE_QUESTIONNAIRE_SINGLE (state, payload) {
+      let questIdx = state.questionnaire.findIndex(function (quest) {
+        return quest.questId === payload.questId
+      })
+      state.questionnaire[questIdx].responses = payload.responses
+      console.log('TCL: MUTATE_QUESTIONNAIRE_SINGLE -> state.questionnaire[questIdx].responses', state.questionnaire[questIdx].responses)
+    },
+    MUTATE_QUESTIONNAIRE (state, payload) {
+      state.questionnaire = payload
+    }
+  },
+  actions: {
+    MUTATE_QUESTIONNAIRE_INDEX ({ commit }, payload) {
+      localStorage.setItem('questionnaireIndex', payload)
+      commit('MUTATE_QUESTIONNAIRE_INDEX', payload)
+    },
+    MUTATE_QUESTIONNAIRE_SINGLE ({ commit }, payload) {
+      commit('MUTATE_QUESTIONNAIRE_SINGLE', payload)
+    },
+    MUTATE_QUESTIONNAIRE ({ commit }, payload) {
+      commit('MUTATE_QUESTIONNAIRE', payload)
+    }
+  }
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import PopupInvitation from './modules/PopupInvitation'
+import PopupMain from './modules/PopupMain'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    PopupInvitation
+    PopupInvitation, PopupMain
   }
 })
